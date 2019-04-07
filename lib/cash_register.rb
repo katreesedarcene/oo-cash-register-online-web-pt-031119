95c85d2546cd15224f74e9c2bbf4bebdabf72b65
Kat B <katreesebarnes@gmail.com>
2019-04-07 20:06:39 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -25,7 +25,7 @@ class CashRegister
   def items
     @items
 end
-  def void_last_transaction
+  def void_last_transaction(price)
     @last_transaction = price * quantity
     @total -= last_transaction
  end
